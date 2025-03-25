1e24ecd75cb3cecdc458f97837d9554f9ecd9fd1
Victor P <vlsmarthen@gmail.com>
2025-03-25 15:01:10 -0400

    ACtually cooked
    added list of human populations

--- people.py
+++ people.py
@@ -1,7 +1,8 @@
 # Add to dependencies!
 import math as m
+import datetime as dt
 # calculate humans by near n
-def human(i, c, g, l, y):
+def human(i:float, c:float, g:float, l:float, y:float):
     # where i is inital population
     # c is children per people, must be >= 0
     # g is time between having kids, in years >= 0
@@ -10,4 +11,16 @@ def human(i, c, g, l, y):
     humans = i * (c/2)**(y/g)*(m.e)**(-y/l) # don't even ask me how this works
     return humans
 
-#print(human(i=8000000000,c=2,g=28,l=80,y=50)) #ok we need to have 4 children or we go extinct
+def humansInYearList(upto, c, g, l):
+    currentYear = dt.datetime.today().year 
+    delta = upto - currentYear + 1 #assuming it is jan 1
+    humanPopulations = []
+    initial = 8000000000
+    for x in range(delta+1):
+        population = human(initial, c, g, l, 1)
+        initial = population
+        humanPopulations.append(population)
+
+    return humanPopulations
+
+print(humansInYearList(2050,3.5,28,85))
