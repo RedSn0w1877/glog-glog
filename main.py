3a017639f56973e78505a30871ca6b83a3e08ea3
Victor P <vlsmarthen@gmail.com>
2025-03-25 13:17:14 -0400

    arson

---
+++ main.py
@@ -0,0 +1,14 @@
+# formula for co2: ΔT=λ⋅ln(C/​C0​)
+# the triangleT thing is like how mmuch is change
+# the half life logo means climate sensitivity, it's like 2.5-4.5 degrees celcious (we in 'MERICA why we using mmetric)
+# the C is co2 concentraion in parts per million
+# the c with a 0 is pre-industrial co2 in ppm like 280ppm
+# bruh more math
+# ok so time for kg -> ppm 
+# Δppm= M*10^-12/(mGt)(f)
+# where triangle ppm is the answer
+# m is the mass of the co2 added in kilograms
+# mGt is the mass of the atmosphere 5.15*10^6 gigatones, aka 515000000000000000 kilograms
+# and f is the fraction of co2
+# screw lines 8-12 just know that 7810000000000 kg of co2 gets u 1ppm
+ 
